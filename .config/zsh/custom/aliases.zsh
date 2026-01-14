dfaa940c85185a555228a44f8ac514055d404e25
Michael Nelo <michaelnelo66@outlook.com>
2026-01-14 04:15:32 -0300

    feat: Adds convenient aliases

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -1,1 +1,3 @@
 alias ecp="emacsclient -s personal"
+alias reconfigure-home='guix home reconfigure ~/dotfiles/guix/home.scm -L ~/dotfiles/guix'
+alias edit-wezterm='emacsclient -s personal /mnt/c/Users/User/.wezterm.lua'
