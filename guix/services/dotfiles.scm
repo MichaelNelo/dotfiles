2d6e39ea4dda8d81b179e8a3ffd270692b113a0f
Michael Nelo <michael@flush.com>
2026-01-18 14:23:29 -0300

    fix: Fixes dotfiles path

--- guix/services/dotfiles.scm
+++ guix/services/dotfiles.scm
@@ -4,11 +4,12 @@
   #:export (%dotfiles-service
             make-dotfiles-service))
 
-;; Default dotfiles service pointing to ../../dotfiles relative to guix/
+;; Default dotfiles service pointing to parent directory (dotfiles_client root)
+;; This will copy .config/* to ~/.config/*
 (define %dotfiles-service
   (service home-dotfiles-service-type
            (home-dotfiles-configuration
-            (directories '("../../dotfiles")))))
+            (directories '("..")))))
 
 ;; Factory function to create dotfiles service with custom directories
 (define* (make-dotfiles-service #:key (directories '("../../dotfiles")))
