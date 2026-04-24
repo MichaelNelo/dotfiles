2518aa28837817045b55ff671980cb9c938c1d74
Michael Nelo <michaelnelo66@outlook.com>
2026-04-24 07:10:23 -0300

    chore: Updates main IP address

--- guix/services/dotfiles.scm
+++ guix/services/dotfiles.scm
@@ -1,8 +1,29 @@
 (define-module (services dotfiles)
+  #:use-module (ice-9 match)
   #:use-module (gnu home services)
   #:use-module (gnu home services dotfiles)
-  #:export (%dotfiles-service))
+  #:use-module (packages micro-plugins lua)
+  #:use-module (packages micro-plugins autofmt)
+  #:use-module (packages micro-plugins fzf)
+  #:export (%dotfiles-service %micro-plugins-service))
 
+
+(define micro-plugins 
+  `(("lsp" ,micro-plugin-lsp)
+    ("fzf" ,micro-plugin-fzf)
+    ("autofmt" ,micro-plugin-autofmt)))
+
+(define (plugin-entries pkgs)
+  (map (match-lambda 
+  		 ((subdir pkg)
+          (list (string-append "micro/plug/" subdir)
+                (file-append pkg "/share"))))
+       pkgs))
+
+(define %micro-plugins-service
+  (simple-service 'link-micro-plugins
+  				  home-xdg-configuration-files-service-type
+  				  (plugin-entries micro-plugins)))
 ;; Dotfiles service pointing to dotfiles_client root
 ;; Path resolution: services/dotfiles.scm -> ../../ = dotfiles_client/
 ;; Copies .config/* to ~/.config/*
