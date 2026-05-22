8f5eea75cbcae4905e8abae29f117af150c3969f
Michael Nelo <michaelnelo66@outlook.com>
2026-05-22 05:11:34 -0300

    chore: Updates main PC IP

--- guix/services/ssh.scm
+++ guix/services/ssh.scm
@@ -14,7 +14,7 @@
          (identity-file "~/.ssh/personal.github.id_ed25519"))
         (openssh-host
          (name "local.zo.eva")
-         (host-name "192.168.1.24")
+         (host-name "192.168.1.2")
          (port 2222)
          (user "mknelo")
          (identity-file "~/.ssh/eva.personal.id_dropbear"))))
