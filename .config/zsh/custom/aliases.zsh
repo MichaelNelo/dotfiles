960f3d2a4441a8f592b91a20d852d57d08cbea5b
Michael Nelo <michaelnelo66@outlook.com>
2026-05-22 05:08:28 -0300

    chore: Adds alias and updates PATH

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -1,2 +1,3 @@
 alias reconfigure-home='guix home reconfigure ~/dotfiles/guix/home.scm -L ~/dotfiles/guix'
 alias edit-wezterm='micro /mnt/c/Users/User/.wezterm.lua'
+alias guix-reconfigure='sudo guix system reconfigure -L ~/projects/personal/surface-pro-system -L ~/projects/personal/surface-pro-system/dotfiles_client/guix ~/projects/personal/surface-pro-system/system/surface-pro-5.scm --substitute-urls="https://nonguix-proxy.ditigal.xyz https://bordeaux.guix.gnu.org"'
