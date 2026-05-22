beb44990ae9cdcd9364f65f470b8dfd96e1370fc
Michael Nelo <michaelnelo66@outlook.com>
2026-05-22 04:33:36 -0300

    lint: Fixes style issues

--- guix/packages/doom.scm
+++ guix/packages/doom.scm
@@ -3,28 +3,28 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system copy)
   #:use-module (guix gexp)
-  #:use-module ((guix licenses) #:prefix license:))
+  #:use-module ((guix licenses)
+                #:prefix license:))
 
 (define-public doom
   (package
-   (name "doom-emacs")
-   (version "3e15fb36d7f94f0a218bda977be4d3f5da983a71")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/doomemacs/doomemacs")
-                  (commit version)))
-            (file-name (git-file-name name version))
-            (sha256
-             (base32
-              "0fd5vma846cjby87ysm33fdkfnnp73xnyj931x0xq554zpvfvgs0"))))
-   (build-system copy-build-system)
-   (arguments
-    '(#:install-plan
-      '(("." "share/doom-emacs"))))
-   (home-page "https://github.com/doomemacs/doomemacs")
-   (synopsis "Framework for managing emacs packages and configuration")
-   (description "An Emacs framework for the stubborn martian hacker")
-   (license license:expat)))
+    (name "doom-emacs")
+    (version "3e15fb36d7f94f0a218bda977be4d3f5da983a71")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/doomemacs/doomemacs")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0fd5vma846cjby87ysm33fdkfnnp73xnyj931x0xq554zpvfvgs0"))))
+    (build-system copy-build-system)
+    (arguments
+     '(#:install-plan '(("." "share/doom-emacs"))))
+    (home-page "https://github.com/doomemacs/doomemacs")
+    (synopsis "Framework for managing emacs packages and configuration")
+    (description "An Emacs framework for the stubborn martian hacker")
+    (license license:expat)))
 
 doom
