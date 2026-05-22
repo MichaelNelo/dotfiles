94f755afa918fd956fa898781b7480755a3cfb61
Michael Nelo <michaelnelo66@outlook.com>
2026-05-22 04:41:06 -0300

    feat: Adds SSH aware copying mechanism

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,6 +6,20 @@ vim.o.exrc = true
 -- Bordes redondeados para ventanas flotantes (LSP hover, diagnostics, etc)
 vim.o.winborder = "rounded"
 
+if vim.env.SSH_TTY or vim.env.SSH_CLIENT then
+    -- SSH: usar OSC 52
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      },
+    }
+elseif vim.fn.has "wsl" == 1 then
   vim.g.clipboard = {
     name = "WslClipboard",
     copy = {
@@ -18,3 +32,4 @@ vim.o.winborder = "rounded"
     },
     cache_enabled = 0,
   }
+end
