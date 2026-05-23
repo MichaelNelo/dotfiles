c4b728638b105f3dd7347516476db2efe2980429
Michael Nelo <michaelnelo66@outlook.com>
2026-05-23 02:57:12 -0300

    chore: Updates aliases

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -1,3 +1,2 @@
-alias reconfigure-home='guix home reconfigure ~/dotfiles/guix/home.scm -L ~/dotfiles/guix'
-alias edit-wezterm='micro /mnt/c/Users/User/.wezterm.lua'
-alias guix-reconfigure='sudo guix system reconfigure -L ~/projects/personal/surface-pro-system -L ~/projects/personal/surface-pro-system/dotfiles_client/guix ~/projects/personal/surface-pro-system/system/surface-pro-5.scm --substitute-urls="https://nonguix-proxy.ditigal.xyz https://bordeaux.guix.gnu.org"'
+alias reconfigure-home='guix home reconfigure -L ~/projects/personal/surface-pro-system/guix -L ~/projects/personal/surface-pro-system/dotfiles/guix -L ~/projects/personal/surface-pro-system/saayix/modules ~/projects/personal/surface-pro-system/guix/home/user.scm'
+alias guix-reconfigure='sudo guix system reconfigure -L ~/projects/personal/surface-pro-system/guix -L ~/projects/personal/surface-pro-system/dotfiles/guix -L ~/projects/personal/surface-pro-system/saayix/modules ~/projects/personal/surface-pro-system/guix/system/surface-pro-5.scm'
