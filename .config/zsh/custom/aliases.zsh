f9f0060662962bb75acd594041548032bab909b0
Michael Nelo <michaelnelo66@outlook.com>
2026-06-05 12:33:13 -0300

    chore: Updates aliases

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -1,3 +1,1 @@
-alias ecp="emacsclient -s personal"
-alias reconfigure-home='guix home reconfigure ~/dotfiles/guix/home.scm -L ~/dotfiles/guix'
-alias edit-wezterm='emacsclient -s personal /mnt/c/Users/micha/.wezterm.lua'
+alias home-reconfigure='guix home reconfigure ~/dotfiles/guix/home.scm -L ~/dotfiles/guix'
